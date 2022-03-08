93bcf21788b852d923f6dce4adc6532869a138da
s.limguy <shuanmlim@icloud.com>
2022-03-08 15:38:32 -0600

    updated user codebase

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,15 @@
 var getUserRepos = function(user) {
     // format the github api url
-    var apiUrl = "http://api.github.com/users/" + user + "/repos";
-
-    // make a request to the url 
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
     });
-};
-getUserRepos("ShuanLim");
+  };
+  
+  getUserRepos("ShuanLim");
+  
